4b4d2509732c59b7805ece0f962b7b37c9283392
shakeyiab <shakeyiabartell@gmail.com>
2024-11-06 01:10:43 -0500

    attempt to commit

--- src/Form.mjs
+++ src/Form.mjs
@@ -2,3 +2,13 @@ export default function Form(props){
     // The component must return some JSX
     return <h1>The Form Component</h1>;
   };
+  export default function Form(props) {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <input type="submit" value="submit" />gthyj
+      </form>
+    </div>
+  );
+}
