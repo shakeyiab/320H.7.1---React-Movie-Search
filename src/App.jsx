2abf2f22f75e6ef26a6ea6068a86f97f09a634ce
shakeyiab <shakeyiabartell@gmail.com>
2024-11-06 01:18:55 -0500

    update app.mjs

--- src/App.jsx
+++ src/App.jsx
@@ -59,5 +59,24 @@ export default function App() {
     </div>
   );
 }
+useEffect(() => {
+  getMovie("Clueless");
+}, []);
 
+return (
+  <div className="App">
+    <Form moviesearch={getMovie} />
+    <MovieDisplay movie={movie} />
+  </div>
+);const getMovie = async(searchTerm) => {
+	try {
+		const response = await fetch(
+			`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+		);
+		const data = await response.json();
+		setMovie(data);
+	} catch(e) {
+		console.error(e)
+	}
+}
 export default App
