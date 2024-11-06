ff9248646d06bd46d9a60f2ae9c7b2b0f7101862
shakeyiab <shakeyiabartell@gmail.com>
2024-11-06 01:13:56 -0500

    State Management

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,28 @@ import MovieDisplay from '../src/MovieDisplay.mjs';
 import Form from '../src/Form.mjs';
 
 export default function App() {
+  // Constant with your API Key
+  const apiKey = "98e3fb1f";
+
+  // State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+  // Function to get movies
+  const getMovie = async(searchTerm) => {
+    // Make fetch request and store the response
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    // Parse JSON response into a JavaScript object
+    const data = await response.json();
+    // Set the Movie state to the received data
+    setMovie(data);
+  };
+
+  // We pass the getMovie function as a prop called moviesearch
   return (
     <div className="App">
-      <Form />
+      <Form moviesearch={getMovie} />
       <MovieDisplay />
     </div>
   );
