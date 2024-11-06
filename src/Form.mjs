24651471486a6479ec4b94763b791819ec2ff5b5
shakeyiab <shakeyiabartell@gmail.com>
2024-11-06 00:41:19 -0500

    Our Components

---
+++ src/Form.mjs
@@ -0,0 +1,4 @@
+export default function Form(props){
+    // The component must return some JSX
+    return <h1>The Form Component</h1>;
+  };
---
+++ src/MovieDisplay.mjs
@@ -0,0 +1,4 @@
+export default function MovieDisplay(props) {
+    // The component must return some JSX
+    return <h1>The MovieDisplay Component</h1>;
+  };
